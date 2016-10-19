f10640c16fd0dc5326e0dc79f4431e8c57b45e78
Ivan Mattie <theguy@ivanmattie.com>
2016-10-19 15:31:19 -0600

    First Commit; basic electron project setup

---
+++ main.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow} = require('electron');
+
+let window;
+
+function createWindow(){
+    window = new BrowserWindow({width: 800, height: 600});
+    window.loadURL(`file://${__dirname}/index.html`);
+
+    window.webContents.openDevTools();
+
+    window.on('closed', () => {
+        window = null;
+    });
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if(window == null){
+        createWindow();
+    }
+});
