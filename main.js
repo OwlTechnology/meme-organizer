0ecc9f85730f9860fe20c677ad4502ebe5d83368
Ivan Mattie <theguy@ivanmattie.com>
2016-10-19 15:38:48 -0600

    Converted basic example to a class

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
 const {app, BrowserWindow} = require('electron');
 
-let window;
+class MemeOrganizer{
 
-function createWindow(){
-    window = new BrowserWindow({width: 800, height: 600});
-    window.loadURL(`file://${__dirname}/index.html`);
+    constructor(){
+        this.window = null;
+        this.targetURL = `file://${__dirname}/index.html`;
 
-    window.webContents.openDevTools();
-
-    window.on('closed', () => {
-        window = null;
-    });
-};
-
-app.on("ready", createWindow);
-
-app.on("window-all-closed", () => {
-    if(process.platform !== 'darwin'){
-        app.quit();
+        app.on("ready", () => this.createWindow());
+        app.on("window-all-closed", () => this.onWindowAllClosed());
     }
-});
 
-app.on("activate", () => {
-    if(window == null){
-        createWindow();
+    createWindow(){
+        if(!this.window){
+            this.window = new BrowserWindow({width: 800, height: 600});
+            this.window.loadURL(this.targetURL);
+
+            this.window.webContents.openDevTools();
+
+            this.window.on("closed", () => this.onWindowClosed());
+        }else{
+            console.warn("A window already exists!");
+        }
     }
-});
+
+    onWindowClosed(){
+        this.window = null;
+    }
+
+    onWindowAllClosed(){
+        if(process.platform !== 'darwin'){
+            app.quit();
+        }
+    }
+
+    onActivate(){
+        if(this.window == null){
+            createWindow();
+        }
+    }
+
+}
+
+new MemeOrganizer();
